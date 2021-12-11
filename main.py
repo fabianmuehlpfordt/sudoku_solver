66c4d50bad34be53ceadfe2bb83e1a2d4f9e3e3a
Fabian Mühlpfordt <fabian@muehlpfordt.eu>
2021-12-11 15:06:47 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Imports
+import pandas as pd
+
+# Side Functions
+
+# Main Function
+if __name__ == "__main__":
+
+    # TODO: read in csv-file instead of image
+
+    # TODO: count rows and cols, must be equal
+
+    # TODO: create empty list for each row and col
